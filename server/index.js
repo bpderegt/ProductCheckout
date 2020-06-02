c30f296e0fd75c51323e93e30e53e03b545f3c4c
Brian de Regt <brian.deregt@gmail.com>
2020-06-01 20:19:01 -0400

    seems like remote instances are referring to local host, trying to have that not happen

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ const PORT = 8673;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 app.use((req, res, next) => {
   console.log(`Incoming ${req.method} request to ${req.path}`);
   next();
@@ -31,8 +31,6 @@ app.get('/products/:id', (req, res) => {
   getProduct(req.params.id, (err, results) => {
     if (err) {
       res.status(500).send(err);
-    // } else if (!results.length) {
-    //   res.status(404).send('Oops! Product not found!');
     } else {
       res.status(200).send(results);
     }
@@ -40,8 +38,6 @@ app.get('/products/:id', (req, res) => {
 });
 
 app.get('/products/:id/stores/:zip', (req, res) => {
-  // console.log(req.params.id, req.params.zip) // <-  5, 23424
-  // res.end()
   getStores(req.params.id, req.params.zip , (err, results) => {
     if (err) {
       res.status(500).send(err);
@@ -53,26 +49,26 @@ app.get('/products/:id/stores/:zip', (req, res) => {
   });
 });
 
-app.post('/products', (req, res) => {
-  // console.log(req.query)
-  postProduct(req.query, (err, succ) => {
-    if (err) {
-      res.status(400).send('could not post query')
-    } else {
-      res.status(201).end()
-    }
-  })
-});
+// app.post('/products', (req, res) => {
+//   // console.log(req.query)
+//   postProduct(req.query, (err, succ) => {
+//     if (err) {
+//       res.status(400).send('could not post query')
+//     } else {
+//       res.status(201).end()
+//     }
+//   })
+// });
 
-app.put('/products/:id', (req, res) => {
-  putProduct(req.query, req.params.id, (err, succ) => {
-    if (err) {
-      res.status(400).send('could not update entry')
-    } else {
-      res.status(204).send(succ)
-    }
-  })
-});
+// app.put('/products/:id', (req, res) => {
+//   putProduct(req.query, req.params.id, (err, succ) => {
+//     if (err) {
+//       res.status(400).send('could not update entry')
+//     } else {
+//       res.status(204).send(succ)
+//     }
+//   })
+// });
 
 // app.delete('products/:id', (req, res) => {
 //   deleteProduct(req.params.id, (err, succ) => {
@@ -84,17 +80,17 @@ app.put('/products/:id', (req, res) => {
 //   })
 // });
 
-app.get('/LEGACY/product/:id/find-store/', (req, res) => {
-  getLEGACYStores(req.params.id, req.query.q, (err, results) => {
-    if (err) {
-      res.status(500).send(err);
-    } else if (!results.length) {
-      res.status(404).send('Store not found');
-    } else {
-      res.status(200).send(results);
-    }
-  });
-});
+// app.get('/LEGACY/product/:id/find-store/', (req, res) => {
+//   getLEGACYStores(req.params.id, req.query.q, (err, results) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else if (!results.length) {
+//       res.status(404).send('Store not found');
+//     } else {
+//       res.status(200).send(results);
+//     }
+//   });
+// });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
